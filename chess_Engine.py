c7b9d24be19d77fa747afc61df20ef79c89c1997
Samuel Trinder <20619907@student.curtin.edu.au>
2021-05-14 16:14:10 +0800

    Added basic setup functions, adding functions to draw board etc

--- chess_Engine.py
+++ chess_Engine.py
@@ -7,14 +7,14 @@
 class gameState():
     def __init__(self):
         self.board = [
-            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB' 'bN', 'bR' ],
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
             ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB' 'wN', 'wR' ]]
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
 
         self.whiteMove = True
         self.moveLog = []
--- chess_Main.py
+++ chess_Main.py
@@ -3,16 +3,57 @@
 #
 
 import pygame as p
-from chess import chess_Engine
+import chess_Engine
 
+p.init()
 Width = Height = 512
 Dimension = 8
 sq_Size = Height // Dimension
 max_FPS = 15
 Images = []
 
+
 def load_Images():
     pieces = ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK',
-          'bP', 'bR', 'bN', 'bB', 'bQ', 'bK']
+              'bP', 'bR', 'bN', 'bB', 'bQ', 'bK']
     for piece in pieces:
         Images[piece] = p.transform.scale(p.image.load('images/' + piece + '.png'), (sq_Size, sq_Size))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((Width, Height))
+    clock = p.time.Clock()
+    screen.fill(p.Color('#00653f'))
+    gs = chess_Engine.gameState()
+    load_Images()
+    running = True
+
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        draw_Gamestate(screen, gs)
+        clock.tick(max_FPS)
+        p.display.flip()
+
+def draw_Gamestate(screen, gs):
+    draw_Board(screen)
+    draw_Pieces(screen, ps.board)
+
+def draw_Board(screen):
+
+
+def draw_Pieces(screen, board):
+    ...
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
